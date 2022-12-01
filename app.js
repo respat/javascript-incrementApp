708109a392c15c0face4a1a03355335d79eadf47
respat <respergerpatrik@gmail.com>
2022-12-01 15:31:57 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+document.getElementById("increase").onclick = function() {
+    count += 1;
+    document.getElementById("counter").innerHTML = count;
+}
+
+document.getElementById("reset").onclick = function() {
+    count = 0;
+    document.getElementById("counter").innerHTML = count;
+}
+
+document.getElementById("decrease").onclick = function() {
+    if (count > 0) {
+        count -= 1;
+        document.getElementById("counter").innerHTML = count;
+    }
+    else {
+        count = 0;
+    }
+}
